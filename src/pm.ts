26794b92c889245ae80c57988e5ec90679bb94c4
Mathis EON <eon@abes.fr>
2021-03-11 13:40:07 +0100

    feature: ENV

--- src/pm.ts
+++ src/pm.ts
@@ -1,36 +1,36 @@
 export class Pm {
-  	info: PmInfo;
-	collectionVariables: collectionVariables
-    constructor() {
-		this.info = new PmInfo()
-		this.collectionVariables = new collectionVariables()
-    } 
+  info: PmInfo;
+  collectionVariables: collectionVariables;
+  constructor() {
+    this.info = new PmInfo();
+    this.collectionVariables = new collectionVariables();
+  }
 }
 
-export  class collectionVariables {
-	collectionVariables: { [key: string]: string; };
-	
-	constructor() {
-		this.collectionVariables = {}
-	}
+export class collectionVariables {
+  collectionVariables: { [key: string]: string };
 
-	set(key: string, val: string) {
-		this.collectionVariables[key] = JSON.parse(val);
-	}
-	
-	get(key: string): string {
-		return JSON.stringify(this.collectionVariables[key])
-	}
-	
-	unset(key: string) {
-		delete this.collectionVariables[key]; 
-	}
+  constructor() {
+    this.collectionVariables = {};
+  }
+
+  set(key: string, val: string) {
+    this.collectionVariables[key] = JSON.parse(val);
+  }
+
+  get(key: string): string {
+    return JSON.stringify(this.collectionVariables[key]);
+  }
+
+  unset(key: string) {
+    delete this.collectionVariables[key];
+  }
 }
 
 export class PmInfo {
-  	requestId: string
-  
-	constructor() {
-		this.requestId = "123e4567-e89b-12d3-a456-426614174000"
-	}
+  requestId: string;
+
+  constructor() {
+    this.requestId = "123e4567-e89b-12d3-a456-426614174000";
+  }
 }
